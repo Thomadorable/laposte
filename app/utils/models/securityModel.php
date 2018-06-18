4547d17b71332ea49ddaf9c1093d9db725dd2938
viococo <violaine.bodolec@gmail.com>
2018-06-18 17:24:43 +0200

    refactoring models + add begin messages

---
+++ app/utils/models/securityModel.php
@@ -0,0 +1,60 @@
+<?php
+
+function login ($login, $password) {
+    $login = htmlspecialchars(trim($login));
+    $password = htmlspecialchars($password);
+    $currentUser = getUserByMail($login);
+
+
+    $hash = hash('sha256', $password);
+
+    $_SESSION['flash'] = [];
+
+    if ($currentUser && $hash === $currentUser->password)  {
+        $_SESSION['name'] = $currentUser->name;
+        $_SESSION['id'] = $currentUser->id;
+
+        header('Location: index.php');
+        exit();
+    } else {
+        $_SESSION['flash']['message'] = 'Les identifiants sont incorrects.';
+        $_SESSION['flash']['type'] = 'error';
+    }
+}
+
+function randId(){
+    $rand = rand(0, 999999999);
+    if (getUserByID($rand)) randId();
+    else return $rand;
+}
+
+function register () {
+    if (!empty($_POST['login']) && !empty($_POST['password'])) {
+
+        $_SESSION['flash'] = [];
+
+        $login = htmlspecialchars(trim($_POST['login']));
+        $currentUser = getUserByMail($login);
+
+        if($currentUser) {
+            $_SESSION['flash']['message'] = "Cet email est déjà utilisé";
+            return false;
+        }
+
+        $bdd = getBdd();
+        $user = [
+            "id" => randId(),
+            "name" => htmlspecialchars(trim($_POST['name'])),
+            "lastname" => htmlspecialchars(trim($_POST['lastname'])),
+            "mail" => htmlspecialchars(trim($_POST['login'])),
+            "password" => hash('sha256', htmlspecialchars(trim($_POST['password']))),
+            "avatar" => "",
+            "idTeam" => 0
+        ];
+
+        $bdd->users->{$user['id']} = $user;
+
+        setBdd($bdd);
+        login($user['mail'], $_POST['password']);
+    } 
+}
---
+++ app/utils/models/userModel.php
@@ -0,0 +1,24 @@
+<?php
+
+function getUserByMail($login) {
+    $bdd = getBdd();
+    $users = $bdd->users;
+    $currentUser = null;
+    foreach ($users as $user) {
+        if ($user->mail === $login) {
+            $currentUser = $user;
+            break;
+        }
+    }
+
+    return $currentUser;
+}
+
+function getUserByID($id) {
+    $bdd = getBdd();
+    $users = $bdd->users;
+    if (property_exists($users, $id)) $currentUser = $users->$id;
+    else $currentUser = null;
+
+    return $currentUser;
+}
